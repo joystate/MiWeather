e058876ea69983456382ae280d49a0bb1bbf0a77
Nadia Yudina <joystate@me.com>
2014-06-15 19:47:19 -0400

    Refactor from view controller

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
         let apiClient = WeatherAPIClient()
         
         apiClient.fetchForecast({ dict in
@@ -23,46 +24,17 @@ class ViewController: UIViewController {
             
             for forecast: AnyObject in forecasts {
                 
-                var humidity: String = String(forecast["humidity"]! as Int)
-                var pressure: String = String(forecast["pressure"]! as Double)
-                var unixDate: Double = forecast["dt"] as Double
-                var dateString: String = self.epochToDate(unixDate)
-
-                var tempDict: Dictionary<String, Double> = forecast["temp"] as Dictionary
-                var dayTemp: String = String(tempDict["day"]! as Double)
-                var nightTemp: String = String(tempDict["night"]! as Double)
-
-                var weatherArray: Array<AnyObject> = forecast["weather"] as Array
-                var identif: String = String(weatherArray[0]["id"] as Int)
-                var weatherDesc: String = weatherArray[0]["description"] as String
-                var iconName: String = weatherArray[0]["icon"] as String
-                
-                var day = Day(date: dateString, dayTemp: dayTemp, nightTemp: nightTemp, pressure: pressure, humidity: humidity, identif: identif, weatherDescription: weatherDesc, iconName: iconName)
-                
+                var day: Day = apiClient.forecastToDay(forecast)
                 self.week.append(day)
-
             }
-            
-            
+
             println(self.week)
 
-            })
+        })
 
     }
     
-    func epochToDate (unixTime: Double) -> String {
-        
-        var interval: NSTimeInterval = unixTime
-        var date = NSDate(timeIntervalSince1970:interval)
-        
-        var dateFmt = NSDateFormatter()
-        var format: String = "dd-MM-yyyy"
-        dateFmt.dateFormat = format
-        var dateString: String = dateFmt.stringFromDate(date)
-        
-        return dateString
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Weather/WeatherAPIClient.swift
+++ Weather/WeatherAPIClient.swift
@@ -33,6 +33,43 @@ class WeatherAPIClient: NSObject {
         task.resume()
     }
     
+    
+    func forecastToDay (forecast: AnyObject) -> Day {
+        
+        var humidity: String = String(forecast["humidity"]! as Int)
+        var pressure: String = String(forecast["pressure"]! as Double)
+        var unixDate: Double = forecast["dt"] as Double
+        var dateString: String = self.epochToDate(unixDate)
+        
+        var tempDict: Dictionary<String, Double> = forecast["temp"] as Dictionary
+        var dayTemp: String = String(tempDict["day"]! as Double)
+        var nightTemp: String = String(tempDict["night"]! as Double)
+        
+        var weatherArray: Array<AnyObject> = forecast["weather"] as Array
+        var identif: String = String(weatherArray[0]["id"] as Int)
+        var weatherDesc: String = weatherArray[0]["description"] as String
+        var iconName: String = weatherArray[0]["icon"] as String
+        
+        var day = Day(date: dateString, dayTemp: dayTemp, nightTemp: nightTemp, pressure: pressure, humidity: humidity, identif: identif, weatherDescription: weatherDesc, iconName: iconName)
+        
+        return day
+    }
+    
+    
+    func epochToDate (unixTime: Double) -> String {
+        
+        var interval: NSTimeInterval = unixTime
+        var date = NSDate(timeIntervalSince1970:interval)
+        
+        var dateFmt = NSDateFormatter()
+        var format: String = "dd-MM-yyyy"
+        dateFmt.dateFormat = format
+        var dateString: String = dateFmt.stringFromDate(date)
+        
+        return dateString
+    }
+
+    
 }
 
 

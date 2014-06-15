d4003bcee650f63b045d6cd6780a1cbac332a184
Nadia Yudina <joystate@me.com>
2014-06-15 00:38:57 -0400

    weather api client with static coordinates

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -11,8 +11,16 @@ import UIKit
 class ViewController: UIViewController {
                             
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let apiClient = WeatherAPIClient()
+        
+        apiClient.fetchForecast({ dict in
+            
+            println(dict)
+            
+            })
+
     }
 
     override func didReceiveMemoryWarning() {
--- Weather/WeatherAPIClient.swift
+++ Weather/WeatherAPIClient.swift
@@ -9,5 +9,32 @@
 import Foundation
 
 class WeatherAPIClient: NSObject {
-
+    
+    let constants = Constants()
+    let urlStringForLocation: String = "/forecast/daily?lat=41&lon=74&cnt=10&mode=json"
+    
+    func fetchForecast(completion: (result: NSDictionary) -> ()) {
+        
+        var urlString: String = constants.baseURL + urlStringForLocation
+        var url: NSURL = NSURL.URLWithString(urlString)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {(data, response, error) in
+            
+            var error: NSError
+            var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+            
+            completion(result: jsonResult)
+            
+            
+            })
+        
+        task.resume() 
+    }
 }
+
+
+
+
+

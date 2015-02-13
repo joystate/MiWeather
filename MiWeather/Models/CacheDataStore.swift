a697d5ec27825d631c033f74d76b2e20b4376f45
Nadia Yudina <joystate@users.noreply.github.com>
2015-02-13 18:14:50 -0500

    fix model name

--- MiWeather/Models/CacheDataStore.swift
+++ MiWeather/Models/CacheDataStore.swift
@@ -31,7 +31,7 @@ public class CacheDataStore: NSObject {
     
     public lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("Some", withExtension: "momd")!
+        let modelURL = NSBundle.mainBundle().URLForResource("Cache", withExtension: "momd")!
         return NSManagedObjectModel(contentsOfURL: modelURL)!
         }()
     
@@ -39,7 +39,7 @@ public class CacheDataStore: NSObject {
         // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
         var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("Some.sqlite")
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("Cache.sqlite")
         var error: NSError? = nil
         var failureReason = "There was an error creating or loading the application's saved data."
         if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {

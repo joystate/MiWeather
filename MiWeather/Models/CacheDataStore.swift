56e8017e8d1e40cc14f716bed33d13dc5dbd8527
Nadia Yudina <joystate@users.noreply.github.com>
2015-02-13 18:26:43 -0500

    setup share persistent store coordinator

--- MiWeather/Models/CacheDataStore.swift
+++ MiWeather/Models/CacheDataStore.swift
@@ -38,8 +38,10 @@ public class CacheDataStore: NSObject {
     public lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
         // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
+        let containerPath = NSFileManager.defaultManager().containerURLForSecurityApplicationGroupIdentifier("group.projects.miweather.Documents")
+        let sqlitePath = NSString(format: "%@/%@", containerPath!, "Cache")
+        let url = NSURL(fileURLWithPath: sqlitePath)
         var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("Cache.sqlite")
         var error: NSError? = nil
         var failureReason = "There was an error creating or loading the application's saved data."
         if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {

3174820a4c1ad2fc8d0c71f9ffb617a472e825aa
Nadia Yudina <joystate@users.noreply.github.com>
2015-02-19 16:10:31 -0500

    add static and dynamic notification

--- MiWeather WatchKit Extension/NotificationController.swift
+++ MiWeather WatchKit Extension/NotificationController.swift
@@ -2,13 +2,56 @@
 //  NotificationController.swift
 //  MiWeather
 //
-//  Created by Nadia Yudina on 2/18/15.
+//  Created by Nadia Yudina on 2/19/15.
 //  Copyright (c) 2015 Nadia Yudina. All rights reserved.
 //
 
 import WatchKit
-import UIKit
+import Foundation
+
 
 class NotificationController: WKUserNotificationInterfaceController {
-   
+
+    @IBOutlet weak var headerLabel: WKInterfaceLabel!
+    
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    
+//    override func didReceiveLocalNotification(localNotification: UILocalNotification, withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
+//        
+//        self.headerLabel.setText("test")
+//        // This method is called when a local notification needs to be presented.
+//        // Implement it if you use a dynamic notification interface.
+//        // Populate your dynamic notification interface as quickly as possible.
+//        //
+//        // After populating your dynamic notification interface call the completion block.
+//        completionHandler(.Custom)
+//    }
+    
+    
+    override func didReceiveRemoteNotification(remoteNotification: [NSObject : AnyObject], withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
+        self.headerLabel.setText("test")
+        // This method is called when a remote notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+        //
+        // After populating your dynamic notification interface call the completion block.
+        completionHandler(.Custom)
+    }
+    
 }
